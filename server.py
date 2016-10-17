6602f72702c571790619f99d32ef85005ac51595
Dominic Zenon <dominiczenon@gmail.com>
2016-10-17 09:21:28 -0400

    added

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import markdown
 
 app = Flask("Wiki")
 
-db = pg.DB(dbname='wiki') 
+db = pg.DB(dbname='wiki')
 
 @app.route('/')
 def wikihome():
@@ -15,8 +15,8 @@ def wikihome():
 def wiki(page_name):
     # id = int(request.form.get('id'))
 
-
-    query = db.query("select title.title, page.page_content from page inner join title on page.title_id = title.id where title = '%s'" % page_name)
+#since there are multipule content pages you have to select from page_id in descending order to get the newest one
+    query = db.query("select title.id as title_id, page.id as page_id, title.title, page.page_content from page inner join title on page.title_id = title.id where title = '%s' order by page_id desc" % page_name)
     result_list = query.namedresult()
     if len(result_list) > 0:
         wiki_page = result_list[0]
@@ -50,11 +50,11 @@ def wiki(page_name):
 
 @app.route('/<page_name>/edit')
 def wikiedit(page_name):
-    query = db.query("select title.title, page.page_content from page inner join title on page.title_id = title.id where title = '%s'" % page_name)
+    query = db.query("select title.id as title_id, page.id as page_id, title.title, page.page_content from page inner join title on page.title_id = title.id where title = '%s' order by page_id desc" % page_name)
 
     page_name = page_name
     result_list = query.namedresult()
-
+    print result_list
     if len(result_list) > 0:
         wiki_page = result_list[0]
 
@@ -76,30 +76,38 @@ def wikiedit(page_name):
 @app.route('/<page_name>/save', methods= ['POST'])
 def newpage(page_name):
 
-    query = db.query("select title.title, page.page_content, title.id as title_id from page inner join title on page.title_id = title.id where title.title = '%s'" % page_name)
+    query = db.query("select title.id as id from page inner join title on page.title_id = title.id where title.title = '%s'" % page_name)
+
     result_list = query.namedresult()
 
     page_content = request.form.get('page_content')
     title = page_name
-    id = request.form.get('id')
+    # id = request.form.get('id')
 
-    if len(result_list) == 0:
+    print "THe length is %s" % len(result_list)
+    if len(result_list) < 1:
         db.insert(
             'title', {
-                'title': page_name
+                'title': page_name,
+
             }
         )
+        query = db.query("select id from title where title.title = '%s'" % page_name)
+        result_list = query.namedresult()
     else:
         pass
     db.insert(
         'page', {
             'page_content': page_content,
             'last_modified_date': "",
-            'title_id': id
+            'title_id': result_list[0].id
         }
     )
 
+
     return redirect('/' + page_name)
+
+
 
 
 if __name__ == '__main__':
